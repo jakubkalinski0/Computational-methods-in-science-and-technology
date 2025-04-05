b8196deeb4eef586d78644810a4246235b474ce9
jakubkalinski0 <kubakalinski2@gmail.com>
2025-04-05 14:27:21 +0200

    Cleaning up Lab2 for better implementation

--- Lab2_Interpolation/src/visualization.h
+++
@@ -1,9 +0,0 @@
-#ifndef VISUALIZATION_H
-#define VISUALIZATION_H
-
-#include "interpolation.h"
-
-// Function declaration
-void saveDataForPlot(InterpolationData *data, double *dd);
-
-#endif // VISUALIZATION_H
